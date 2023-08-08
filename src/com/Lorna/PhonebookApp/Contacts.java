f375f49c96af556da78f7e36de5a8df44c78be0a
akothouma <lornaakothpb@gmail.com>
2023-08-08 13:49:05 +0300

    first commit

---
+++ src/com/Lorna/PhonebookApp/Contacts.java
@@ -0,0 +1,4 @@
+package com.Lorna.PhonebookApp;
+
+public class Contacts {
+}
---
+++ src/com/Lorna/PhonebookApp/Main.java
@@ -0,0 +1,8 @@
+package com.Lorna.PhonebookApp;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
